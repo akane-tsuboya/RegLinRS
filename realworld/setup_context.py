51622830957f51db78a64cd80b1f8132e17c7491
akane-minami <akane03730913@gmail.com>
2023-11-16 15:33:23 +0900

    冗長な箇所の修正

--- realworld/setup_context.py
+++ realworld/setup_context.py
@@ -1,7 +1,4 @@
 """データのセットアップをおこなうモジュール"""
-from realworld.data_sampler import sample_mushroom_data
-from realworld.data_sampler import sample_stock_data
-from realworld.data_sampler import sample_jester_data
 from realworld.data_sampler import sample_artificial_data
 from realworld.data_sampler import sample_mixed_artificial_data
 
@@ -21,28 +18,7 @@ class ContextData(object):
             DATA NAME ERROR: data_typeがどれも当てはまらない場合
         """
 
-        if data_type == 'mushroom':
-            dataset, opt_mushroom, exp_rewards = sample_mushroom_data(num_contexts)
-            opt_rewards, opt_actions = opt_mushroom
-        elif data_type == 'financial':
-            num_actions = 8
-            context_dim = 21
-            num_contexts = min(3713, num_contexts)
-            noise_stds = [0.01 * (i + 1) for i in range(num_actions)]
-            dataset, opt_financial = sample_stock_data(context_dim,
-                                num_actions, num_contexts,
-                                noise_stds, shuffle_rows=True)
-            opt_rewards, opt_actions = opt_financial
-        elif data_type == 'jester':
-            num_actions = 8
-            context_dim = 32
-            num_contexts = min(19181, num_contexts)
-            dataset, opt_jester,exp_rewards = sample_jester_data(context_dim,
-                                num_actions, num_contexts,
-                                shuffle_rows=True,
-                                shuffle_cols=True)
-            opt_rewards, opt_actions = opt_jester
-        elif data_type.startswith('artificial'):
+        if data_type.startswith('artificial'):
             num_actions = 8
             context_dim = 128
             dataset, opt_artificial, exp_rewards = sample_artificial_data(data_type, num_contexts, context_dim)
@@ -63,16 +39,7 @@ class ContextData(object):
             num_actions(int):行動数
             context_dim(int):特徴量の次元
         """
-        if data_type == 'mushroom':
-            num_actions = 2
-            context_dim = 117
-        elif data_type == 'financial':
-            num_actions = 8
-            context_dim = 21
-        elif data_type == 'jester':
-            num_actions = 8
-            context_dim = 32
-        elif data_type.startswith('artificial'):
+        if data_type.startswith('artificial'):
             num_actions = 8
             context_dim = 128
         elif data_type.startswith('mixed_artificial'):
--- sim/real_sim.py
+++ sim/real_sim.py
@@ -145,13 +145,7 @@ class ContextualBanditSimulator(object):
                 success_acc = 1 if chosen_arm == opt_actions[step] else 0#真のgreedy(Accuracy)
                 success_greedy = 1 if chosen_arm == np.argmax(theta_hat) else 0#主観greedy
 
-                if self.data_type == 'mushroom':
-                  theta_error = mean_squared_error([row[opt_actions[step]] for row in exp_rewards], theta_hat, squared=False) #True:MSE, False:RMSE
-                  #theta_error = mean_absolute_error([row[opt_actions[step]] for row in exp_rewards], theta_hat)
-                elif self.data_type == 'jester':
-                  theta_error = mean_squared_error(exp_rewards[step], theta_hat, squared=False)
-                  #theta_error = mean_absolute_error(exp_rewards[step], theta_hat)
-                elif self.data_type.startswith('artificial'):
+                if self.data_type.startswith('artificial'):
                   theta_error = mean_squared_error(exp_rewards[step], theta_hat, squared=False)
                   #theta_error = mean_absolute_error(exp_rewards[step], theta_hat)
                 elif self.data_type.startswith('mixed_artificial'):
@@ -224,81 +218,3 @@ class ContextualBanditSimulator(object):
   def run(self) -> None:
         """一連のシミュレーションを実行"""
         self.run_sim() # 結果のlog採取
-        #self.plots() # plot は別
-
-  def plots(self) -> None:
-        """結果データのプロット"""
-        mpl.rcParams['axes.xmargin'] = 0
-        mpl.rcParams['axes.ymargin'] = 0
-        mpl.rcParams['font.family'] = 'Noto Sans CJK JP'
-
-        time_now = datetime.datetime.now()
-        results_dir = 'png/{0:%Y%m%d%H%M}/'.format(time_now)
-        os.makedirs(results_dir, exist_ok=True)
-
-        for i, data_name in enumerate(['rewards', 'regrets','accuracy', 'greedy_rate','errors', 'entropy_of_reliability']):
-            fig = plt.figure(figsize=(12, 8))
-            ax = fig.add_subplot(111)
-
-            for j, policy_name in enumerate(self.policy_name):
-                cmap = plt.get_cmap("tab10")
-                if data_name == 'greedy_rate' or data_name =='accuracy':
-                    """通常ver"""
-                    #ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), self.result_list.at[j, data_name], label=policy_name,linewidth=1.5, alpha=0.8)
-                    """移動平均ver"""
-                    b=np.ones(30)/30.0
-                    y3=np.convolve(self.result_list.at[j, data_name], b, mode='same')
-                    ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), y3, label=policy_name, color=cmap(j), linewidth=1.5, alpha=0.8)
-                    ax.set_ylim([0.2, 1.1])
-                elif data_name =='errors':
-                    """通常ver"""
-                    #ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), self.result_list.at[j, data_name], label=policy_name, linewidth=1.5, alpha=0.8)
-                    #ax.fill_between(x=np.linspace(1, self.n_steps, num=self.n_steps), y1=self.result_list.at[j, 'min_'+data_name], y2=self.result_list.at[j, 'max_'+data_name], alpha=0.1)
-                    """移動平均ver"""
-                    b=np.ones(10)/10.0
-                    y3=np.convolve(self.result_list.at[j, data_name], b, mode='same')#移動平均
-                    ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), y3, label=policy_name+"moving_average", color=cmap(j), linewidth=1.5, alpha=0.8)
-                    ax.set_ylim([0,1.0])
-                elif data_name == 'entropy_of_reliability':
-                    if 'LinRS' in policy_name:
-                      ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), self.result_list.at[j, data_name], label=policy_name, linewidth=1.5, alpha=0.8)
-                      ax.set_ylim([0,1.1])
-                else:
-                    ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), self.result_list.at[j, data_name], label=policy_name, linewidth=1.5, alpha=0.8)
-                    #ax.fill_between(x=np.linspace(1, self.n_steps, num=self.n_steps), y1=self.result_list.at[j, 'min_'+data_name], y2=self.result_list.at[j, 'max_'+data_name], alpha=0.1)
-            ax.set_xlabel('step',fontsize=23)
-            if data_name == 'rewards':
-              ax.set_ylabel('reward',fontsize=23)
-            elif data_name == 'regrets':
-              ax.set_ylabel('regret',fontsize=23)
-            elif data_name == 'greedy_rate':
-              ax.set_ylabel('greedy rate',fontsize=23)
-            elif data_name == 'errors':
-              ax.set_ylabel('error',fontsize=23)
-            elif data_name == 'entropy_of_reliability':
-              ax.set_ylabel('entropy of reliability',fontsize=23)
-            else:
-              ax.set_ylabel(data_name,fontsize=23)
-            ax.spines["top"].set_linewidth(2)
-            ax.spines["left"].set_linewidth(2)
-            ax.spines["bottom"].set_linewidth(2)
-            ax.spines["right"].set_linewidth(2)
-
-            if data_name == 'greedy_rate' or data_name =='accuracy':
-              leg=ax.legend(loc='lower right', fontsize=23)
-            elif data_name == 'entropy_of_reliability':
-              leg=ax.legend(loc='lower left', fontsize=23)
-            else:
-              leg=ax.legend(loc='upper left', fontsize=23)
-
-            plt.tick_params(labelsize=10)
-            ax.grid(axis='y')
-
-            #plt.show()
-            #path = os.getcwd()#現在地
-            #results_dir = os.path.join(path, 'png/{0:%Y%m%d%H%M}/'.format(time_now))#保存場所
-            fig.savefig(results_dir + data_name, bbox_inches='tight', pad_inches=0)
-
-
-
-        #plt.clf()
